6bed0f88886b880ea68e563756023e3659f50ba5
kohsuke.yuasa <kohsuke.yuasa@pol.co.jp>
2022-12-12 19:25:24 +0900

    feat: update lib.rs

--- wasm-audio/src/lib.rs
+++ wasm-audio/src/lib.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use wasm_bindgen::prelude::*;
 use rustfft::{FftPlanner, Fft, num_complex::Complex};
 
@@ -50,16 +52,18 @@ impl RingBuffer<T> {
   }
 
   pub fn num_readable(&self) {
-    self.read_pos <=self.write_pos
-    ? self.write_pos - self.read_pos
-    : self.write_pos + (self.capacity + 1) - read_pos
+    if self.read_pos <=self.write_pos {
+      self.write_pos - self.read_pos
+    } else {
+      self.write_pos + (self.capacity + 1) - self.read_pos
+    }
   }
 
   pub fn num_writable(&self) {
-    self.capacity - num_readable()
+    self.capacity - self.num_readable()
   }
 
-  pub fn read(&mut self, dest: &mut [f32], length: usize) -> boolean {
+  pub fn read(&mut self, dest: &mut [f32], length: usize) -> bool {
     if self.num_readable() < length {
       return false;
     }
@@ -68,19 +72,19 @@ impl RingBuffer<T> {
     let copy1 = std::cmp::min(buffer_length - self.read_pos, length);
 
 
-    dest[0..copy1].clone_from_slice(self.buffer[self.read_pos..self.read_pos + copy1]);
+    dest[0..copy1].clone_from_slice(&self.buffer[self.read_pos..self.read_pos + copy1]);
 
     if copy1 == length {
       return true;
     }
 
     let copy2 = std::cmp::max(length, copy1) - copy1;
-    dest[copy1..copy1+copy2].clone_from_slice(self.buffer[0..copy2]);
+    dest[copy1..copy1+copy2].clone_from_slice(&self.buffer[0..copy2]);
 
     true
   }
 
-  pub fn write(&mut self, src: &[f32], length: usize) -> boolean {
+  pub fn write(&mut self, src: &[f32], length: usize) -> bool {
     if self.num_readable() < length {
       return false;
     }
@@ -89,13 +93,13 @@ impl RingBuffer<T> {
     let copy1 = std::cmp::min(buffer_length - self.write_pos, length);
 
     if copy1 == length {
-      self.buffer[self.write_pos..self.write_pos + copy1].clone_from_slice(src[0..copy1]);
+      self.buffer[self.write_pos..self.write_pos + copy1].clone_from_slice(&src[0..copy1]);
       self.write_pos += copy1;
       return true;
     }
 
     let copy2 = length - copy1;
-    self.buffer[0..copy2].clone_from_slice(src[copy1..copy1 + copy2]);
+    self.buffer[0..copy2].clone_from_slice(&src[copy1..copy1 + copy2]);
     self.write_pos = copy2;
     true
   }
@@ -117,23 +121,25 @@ impl RingBuffer<T> {
     let buffer_length = self.capacity + 1;
 
     let write_start =
-    (self.write_pos >= overlap_size)
-    ?   self.write_pos - overlap_size
-    :   (buffer_length - (overlap_size - self.write_pos));
+    if self.write_pos >= overlap_size {
+      self.write_pos - overlap_size
+    } else {
+      buffer_length - (overlap_size - self.write_pos)
+    };
 
     let copy1 = std::cmp::min(buffer_length - write_start, overlap_size);
 
-    self.buffer[write_start..write_start + copy1].clone_from_slice(src[0..copy1]);
+    self.buffer[write_start..write_start + copy1].clone_from_slice(&src[0..copy1]);
 
     if copy1 != overlap_size {
       let copy2 = overlap_size - copy1;
-      self.buffer[0..copy2].clone_from_slice(src[copy1..copy1 + copy2]);
+      self.buffer[0..copy2].clone_from_slice(&src[copy1..copy1 + copy2]);
     }
 
-    write(src[overlap_size..length], num_to_write_new)
+    self.write(&src[overlap_size..length], num_to_write_new)
   }
 
-  pub fn fill(&mut self, length: usize, value: T) -> boolean {
+  pub fn fill(&mut self, length: usize, value: T) -> bool {
     if self.num_writable() < length {
       return false;
     }
@@ -141,7 +147,7 @@ impl RingBuffer<T> {
     let buffer_length = self.capacity + 1;
     let copy1 = std::cmp::min(buffer_length - self.write_pos, length);
 
-    self.buffer[self.write_pos, self.write_pos + copy1].fill(value);
+    self.buffer[self.write_pos..self.write_pos + copy1].fill(value);
 
     if copy1 == length {
       self.write_pos += copy1;
@@ -154,23 +160,48 @@ impl RingBuffer<T> {
 
     true
   }
+
+  pub fn discard(&mut self, length: usize) -> bool {
+    if self.num_readable() < length {
+      return false;
+    }
+
+    let buffer_length = self.capacity + 1;
+    let discard1 = std::cmp::min(buffer_length - self.read_pos, length);
+
+    if discard1 == length {
+      self.read_pos += length;
+      return true;
+    }
+
+    let discard2 = length - discard1;
+    self.read_pos = discard2;
+
+    true
+  }
+
+  pub fn discard_all(&mut self) {
+    self.discard(self.num_readable());
+  }
 }
+
+type Cmp = Complex<f32>;
 
 #[wasm_bindgen]
 pub struct WasmProcessor {
   sample_rate: usize,
-  fft_planner: FftPlanner,
-  fft: Arc<dyn Fft<T>>,
-  ifft: Arg<dyn Fft<T>>,
+  block_size: usize,
+  fft: Arc<dyn Fft<f32>>,
+  ifft: Arc<dyn Fft<f32>>,
   fft_size: usize,
   overlap_size: usize,
   input_ring_buffer: RingBuffer<f32>,
   output_ring_buffer: RingBuffer<f32>,
-  signal_buffer: Vec<Complex>,
-  frequency_buffer: Vec<Complex>,
-  cepstrum_buffer: Vec<Complex>,
-  tmp_fft_buffer: Vec<Complex>,
-  tmp_fft_buffer2: Vec<Complex>,
+  signal_buffer: Vec<Cmp>,
+  frequency_buffer: Vec<Cmp>,
+  cepstrum_buffer: Vec<Cmp>,
+  tmp_fft_buffer: Vec<Cmp>,
+  tmp_fft_buffer2: Vec<Cmp>,
   tmp_phase_buffer: Vec<f32>,
   window: Vec<f32>,
   prev_input_phases: Vec<f32>,
@@ -179,20 +210,20 @@ pub struct WasmProcessor {
   analysis_frequencies: Vec<f32>,
   synthesis_magnitude: Vec<f32>,
   synthesis_frequencies: Vec<f32>,
+  tmp_buffer: Vec<f32>,
+  wet_buffer: Vec<f32>,
   dry_wet: f32,
   formant: f32,
   pitch: f32,
   output_gain: f32,
-  envelope_order,
+  envelope_order: usize,
   input_level: f32,
   output_level: f32,
-  tmp_buffer: Vec<f32>,
-  wet_buffer: Vec<f32>,
-}
+};
 
 #[wasm_bindgen]
 impl WasmProcessor {
-  pub fn new(sample_rate: usize) -> WasmProcessor {
+  pub fn new(sample_rate: usize, block_size: usize) -> WasmProcessor {
     utils::set_panic_hook();
 
     unsafe { MIN_DECIBEL_GAIN = decibel_to_gain(MIN_DECIBEL); };
@@ -203,14 +234,66 @@ impl WasmProcessor {
     let fft_planner = FftPlanner::new();
     let fft = fft_planner.plan_fft_forward(fft_size);
     let ifft = fft_planner.plan_fft_backward(fft_size);
+
+    let signal_buffer = vec![Cmp::new(0, 0), fft_size];
+    let frequency_buffer = vec![Cmp::new(0, 0), fft_size];
+    let cepstrum_buffer = vec![Cmp::new(0, 0), fft_size];
+
+    let window = vec![0.0f32, fft_size];
+    for (i, elem) in window.iter_mut().enumerate() {
+      let omega: f32 = 2.0f32 * std::f32::consts::PI * i / fft_size;
+      elem = 0.5f32 * (1.0f32 - omega.cos());
+    }
+
+    let input_ring_buffer = RingBuffer::<f32>::new(fft_size);
+    input_ring_buffer.discard_all();
+    input_ring_buffer.fill(fft_size - overlap_size);
+
+    let output_ring_buffer = RingBuffer::<f32>::new(fft_size + block_size);
+    output_ring_buffer.discard_all();
+    output_ring_buffer.fill(fft_size + block_size - overlap_size);
+
+    let tmp_buffer = vec![0.0f32, fft_size];
+    let wet_buffer = vec![0.0f32, block_size];
+
+    let tmp_fft_buffer = vec![Cmp::new(0, 0), fft_size];
+    let tmp_fft_buffer2 = vec![Cmp::new(0, 0), fft_size];
+    let tmp_phase_buffer = vec![Cmp::new(0, 0), fft_size];
+    let prev_input_phases = vec![Cmp::new(0, 0), fft_size];
+    let prev_output_phases = vec![Cmp::new(0, 0), fft_size];
+    let analysis_magnitude = vec![Cmp::new(0, 0), fft_size];
+    let analysis_frequencies = vec![Cmp::new(0, 0), fft_size];
+    let synthesis_magnitude = vec![Cmp::new(0, 0), fft_size];
+    let synthesis_frequencies = vec![Cmp::new(0, 0), fft_size];
+
     let d = WasmProcessor {
       sample_rate,
-      fft_planner,
+      block_size,
       fft,
       ifft,
       fft_size,
       overlap_size,
-      input_ring_buffer: RingBuffer<f32>::new(fft_size +
+      input_ring_buffer,
+      output_ring_buffer,
+      signal_buffer,
+      frequency_buffer,
+      cepstrum_buffer,
+      tmp_fft_buffer,
+      tmp_fft_buffer2,
+      tmp_phase_buffer,
+      window,
+      prev_input_phases,
+      prev_output_phases,
+      analysis_magnitude,
+      analysis_frequencies,
+      synthesis_magnitude,
+      synthesis_frequencies,
+      tmp_buffer,
+      wet_buffer,
+      dry_wet: 0.5,
+      formant: 0.5,
+      pitch: 0.5,
+      output_gain: 0.0,
       input_level: MIN_DECIBEL,
       output_level: MIN_DECIBEL,
     };
